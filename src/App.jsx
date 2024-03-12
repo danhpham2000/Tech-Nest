a7127b3f697d868c91c48f89ab7c2fdc89a20edd
Danh Pham <115748225+danhpham2000@users.noreply.github.com>
2024-03-12 14:29:28 -0700

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./index.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <h1>hello</h1>
+    // <Router>
+    //   <div className="App">
+    //     <h1>Home</h1>
+
+    //     {/* <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/create" element={<Create />} />
+    //       <Route path="/blogs/:id" element={<BlogDetails />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/signup" element={<Signup />} />
+
+    //       <Route path="*" element={<NotFound />} /> */}
+    //     </Routes>
+    //   </div>
+    // </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="main-title">Tech Corner</h1>
+    </div>
+  );
+};
+
+export default Navbar;
